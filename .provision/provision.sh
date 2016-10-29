f800962ef542a12964b2c0282a2fda0011d1d897
Christopher Lee <chmalee@ucsc.edu>
2016-10-28 17:59:58 -0700

    Cleanup unused sections of vagrantfile, prepping provision.sh for both initial setup and re-syncing

--- .provision/provision.sh
+++ .provision/provision.sh
@@ -1,4 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/bash
+set -u -e -o pipefail # fail on all errors and in pipes
+
+# vars we will want
+LOC=/usr/local
+APACHEDIR=/var/www
+CGIS=$APACHEDIR/cgi-bin
+HTDOCS=$APACHE/html
 
 echo "installing vim, wget, rsync, apache"
 sudo yum install -y vim wget rsync httpd git
@@ -8,7 +15,7 @@ sudo yum groupinstall -y Development tools
 sudo yum install -y zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel
 
 # check if fresh install or just re-provisioning
-if [ ! -e /usr/local/bin/python3.5]; then
+if [ ! -e $LOC/bin/python3.5]; then
    if [ ! -d Python-3.5.2 ]; then # don't redownload unless we need to
       if [ ! -e Python-3.5.2.tgz ]; then
          wget https://www.python.org/ftp/python/3.5.2/Python-3.5.2.tgz
@@ -16,10 +23,9 @@ if [ ! -e /usr/local/bin/python3.5]; then
       tar -xvzf Python-3.5.2.tgz
    fi
    cd Python-3.5.2
-   ./configure --prefix=/usr/local # allow multiple versions of python
+   ./configure --prefix=$LOC # allow multiple versions of python
    make altinstall
-else # check for required python dependencies as well
-   # download python packages
+else # otherwise just download what we need
    pip3.5 install requests pandas bs4
 fi
 
@@ -32,7 +38,7 @@ sudo systemctl status httpd
 # now we need to to configure /var/www directories to serve our app
 rm -rf /var/www # clean directories first
 sudo mkdir -p /var/www/{cgi-bin,html}
-ln -s /vagrant/src /var/www/cgi-bin/
-ln -s /vagrant/htdocs /var/www/html
-ln -s /vagrant/css /var/www/html/css
-ln -s /vagrant/js /var/www/html/js
+ln -s /vagrant/src $CGIS
+ln -s /vagrant/htdocs $HTDOCS
+ln -s /vagrant/css $HTDOCS/css
+ln -s /vagrant/js $HTDOCS/js
