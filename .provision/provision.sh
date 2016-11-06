af8b29b458ed558225d292d90faf4ab0e3337d91
Christopher Lee <chmalee@ucsc.edu>
2016-11-05 21:35:23 -0700

    Allow options to script

--- .provision/provision.sh
+++ .provision/provision.sh
@@ -7,15 +7,17 @@ APACHEDIR=/var/www
 CGIS=$APACHEDIR/cgi-bin
 HTDOCS=$APACHEDIR/html
 
+initialSetup () 
+{
 echo "installing vim, wget, rsync, apache"
-sudo yum install -y vim wget rsync httpd git
+sudo yum install -y vim wget rsync httpd git setroubleshoot
 
 echo "setting up python"
 sudo yum groupinstall -y Development tools
 sudo yum install -y zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel
 
 # Check if fresh install or just re-provisioning
-if [ ! -e $LOC/bin/python3.5]; then
+if [ ! -e $LOC/bin/python3.5 ]; then
    if [ ! -d Python-3.5.2 ]; then # don't redownload unless we need to
       if [ ! -e Python-3.5.2.tgz ]; then
          wget https://www.python.org/ftp/python/3.5.2/Python-3.5.2.tgz
@@ -26,30 +28,70 @@ if [ ! -e $LOC/bin/python3.5]; then
    ./configure --prefix=$LOC # allow multiple versions of python
    make altinstall
 else # otherwise just download what we need
-   pip3.5 install requests pandas bs4
+   $LOC/bin/pip3.5 install requests pandas bs4
 fi
+}
 
+# there should be a better way to check if apache is running :(
+setupApache ()
+{
 echo "starting apache and enabling on vm boot"
-if [ ! $(service httpd status) =~ "running" ]; then 
+pgrep httpd > /dev/null && echo running > /dev/null
+if [ $? -ne 0 ]; then 
    sudo systemctl enable httpd
    sudo systemctl start httpd
    sudo systemctl status httpd
+else
+   echo "apache already running"
 fi
+}
+
+# get rid of selinux issues
+fixSELinux ()
+{
+sudo chcon  --user system_u --type httpd_sys_content_t -RvL $HTDOCS
+sudo chcon  --user system_u --type httpd_sys_script_exec_t -RvL $CGIS
+# python urlib can run fine from command line but not by apache, fix with the following
+sudo setsebool -P httpd_can_network_connect 1
+}
 
 # Now we need to to configure /var/www directories to serve our app
-# Clean directories first
 # This will always be necessary, even if we are just running vagrant reload
 # as it is assumed there are changes in the shared directory and symlinks may
 # need to be updated.
+syncFiles () 
+{
 sudo rm -rf /var/www 
 sudo mkdir -p $APACHEDIR/{cgi-bin,html}
 sudo ln -s /vagrant/src $CGIS
 sudo ln -s /vagrant/htdocs $HTDOCS
 sudo ln -s /vagrant/css $HTDOCS/css
 sudo ln -s /vagrant/js $HTDOCS/js
-sudo chown -R apache $APACHEDIR $CGIS $HTDOCS
+sudo chown -R apache:apache $APACHEDIR $CGIS $HTDOCS
+}
 
-# TODO: Allow args to this script so we can specify whether we need full setup, just 
-#      update to existing files, or new files that need new symlinks.
+# main
+trap errorHandler ERR
 
+if [ $# -eq 0 ]; then
+   echo "running full install"
+   initialSetup
+   setupApache
+   syncFiles
+   fixSELinux
+   exit 0
+fi
+
+while getopts "s" opt; do
+   case $opt in
+      s)
+         echo "Only syncing new files, hopefully apache and python are already installed!"
+         syncFiles
+         exit 0
+      \?)
+         echo "invalid opton: -$OPTARG"
+         ;;
+   esac
+done
+#TODO: Add flag to detect if full install has been ran yet, don't allow s option if so
 echo "VM finished setup. Please check that you can load the home page at http://127.0.0.1:8080"
